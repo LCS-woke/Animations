cf946f88eafec9262b514a9717827117cb76c7e7
LCS-woke <149812783+LCS-woke@users.noreply.github.com>
2024-02-02 08:43:30 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by William Oke on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
